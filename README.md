aec63f93142f9ce2d169f678aeade25be428b544
Dishon Merkhai <merkhai@dishon.ca>
2020-04-17 00:46:42 -0400

    Add -s, -c, -i arguments functionality. Early implementation of ping summary functionality.

--- README.md
+++ README.md
@@ -5,14 +5,17 @@ This program is a Rust implementation of the ping command found in UNIX.
 ## Roadmap
 
 - [x] Argument parsing
-- [x] Add support for both IPv4 and IPv6
+- [x] Support IPv4 and IPv6
 - [x] Allow to set TTL as an argument
 - [x] Support size of payload (in bytes) setting 
 - [x] Send ICMP echo requests in an infinite loop to the target while receiving echo reply message (have a periodic delay)
 - [x] Report RTT time for each sent message
 - [x] Support TTL (IPv4, IPv6)
 - [x] Report the ICMP messages that are exceeding the time set by the TTL argument
-- [x] Implement -s argument (specify number of data bytes sent)
+- [x] Implement `-s packet_size` argument (specify number of data bytes sent)
 - [x] Report Loss for each sent message
+- [x] Implement `-c count` argument(stop after sending (and receiving) count ECHO_RESPONSE packets.)
+- [x] Implement `-i max_rtt` argument (duration between sending packets)
+- [ ] Implement ping summary
 - [ ] Implement any interesting arguments in the UNIX ping command
-    - [ ] Implement -c count (stop after sending (and receiving) count ECHO_RESPONSE packets.)
+
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::net::{IpAddr};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "ping-util-rs", author = "Dishon Merkhai", no_version, about = "This program is a Rust implementation of the UNIX ping command")]
-struct Opt {
+pub struct Opt {
   // Activate debug mode
   #[structopt(short, long, help ="Activate debug mode")]
   debug: bool,
@@ -22,22 +22,32 @@ struct Opt {
   #[structopt(short = "c", long = "count", default_value = "-1", help = "Terminates after sending (and receiving) count ECHO_RESPONSE packets")]
   max_packets: i32,
 
+  // 
   #[structopt(short = "s", default_value = "56", help = "Specify the number of data bytes to be sent.  The default is 56,
              which translates into 64 ICMP data bytes when combined with the 8
              bytes of ICMP header data.  This option cannot be used with ping
              sweeps.")]
   packet_size: i32, // TODO: should be u32, but the library implementation has i32
 
+  // 
+  #[structopt(short = "i", long = "rtt", default_value = "1000", help = "Wait wait_time milliseconds between sending each packet.  The default is to
+             wait for one second between each packet.")]
+  max_rtt: u64,
 }
 
+pub fn summary(_opt: &Opt, _icmp_seq: u32, _failed_packets: u32) {
+  println!("\n--- {} ping statistics ---", _opt.ip);
+  println!("{} packets transmitted, {} packets received, {:.3?}% packet loss", _icmp_seq, (_icmp_seq - _failed_packets), ((_failed_packets/_icmp_seq)*100));
+  println!("round-trip min/avg/max/stddev = 0.000/0.000/0.000/0.000 ms") // {:.3?}
+}
 
 fn main() {
   let opt = Opt::from_args();
-  println!("{:#?}", opt);
+  // println!("{:#?}", opt);
 
   // experimental implementation provided by fastping_rs documentation
   env_logger::init();
-  let (pinger, results) = match ping_util_rs::Pinger::new(None, Some(opt.packet_size), Some(opt.ttl), opt.ip.is_ipv4()) {
+  let (pinger, results) = match ping_util_rs::Pinger::new(Some(opt.max_rtt), Some(opt.packet_size), Some(opt.ttl), opt.ip.is_ipv4()) {
       Ok((pinger, results)) => (pinger, results),
       Err(e) => panic!("Error creating pinger: {}", e)
   };
@@ -53,23 +63,29 @@ fn main() {
 
   println!("PING {} ({}): {} data bytes", opt.ip, opt.ip, pinger.get_size());
 
-  loop {
-      match results.recv() {
-          Ok(result) => {
-              icmp_seq += 1;
-              match result {
-                  ping_util_rs::PingResult::Idle{addr} => {
-                      log::error!("TTL Time Exceeded from {}: icmp_seq={} payload={}B", addr, icmp_seq, send_size);
-                      failed_packets += 1;
-                  },
-                  ping_util_rs::PingResult::Receive{addr, rtt} => {
-                      println!("{} bytes from {}: icmp_seq={} ttl={} rtt={:.5?} loss={}%", send_size, addr, icmp_seq, opt.ttl, rtt, ((failed_packets/icmp_seq)*100));
-                  }
-              }
-          },
-          Err(_) => panic!("Worker threads disconnected before the solution was found!"),
-      }
-  }
+  let mut x: i32 = 0;
 
+  while opt.max_packets != x {
+    match results.recv() {
+        Ok(result) => {
+            icmp_seq += 1;
+            match result {
+                ping_util_rs::PingResult::Idle{addr} => {
+                    log::error!("TTL Time Exceeded from {}: icmp_seq={} payload={}B", addr, icmp_seq, send_size);
+                    failed_packets += 1;
+                },
+                ping_util_rs::PingResult::Receive{addr, rtt} => {
+                    println!("{} bytes from {}: icmp_seq={} ttl={} rtt={:.5?} loss={}%", send_size, addr, icmp_seq, opt.ttl, rtt, ((failed_packets/icmp_seq)*100));
+                }
+            }
+        },
+        Err(_) => panic!("Worker threads disconnected before the solution was found!"),
+    }
+    x += 1;
+  }
+  // stop the pinger device
+  pinger.stop_pinger();
+
+  summary(&opt, icmp_seq, failed_packets);
 
 }
